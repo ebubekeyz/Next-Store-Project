58b865b2f7483e20b342a2625ae4fe97b74d8674
ebubekeyz <ebubeofforjoe@gmail.com>
2025-12-08 08:00:27 -0800

    fifth commit

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,7 +1,23 @@
-import React from "react";
+import FormContainer from "@/components/form/FormContainer";
+import { IconButton } from "@/components/form/Buttons";
+import { deleteProductAction } from "@/utils/actions";
 
-const page = () => {
-  return <div>side</div>;
-};
+return (
+  <>
+    <TableCell className="flex items-center gap-x-2">
+      <Link href={`/admin/products/${productId}/edit`}>
+        <IconButton actionType="edit"></IconButton>
+      </Link>
+      <DeleteProduct productId={productId} />
+    </TableCell>
+  </>
+);
 
-export default page;
+function DeleteProduct({ productId }: { productId: string }) {
+  const deleteProduct = deleteProductAction.bind(null, { productId });
+  return (
+    <FormContainer action={deleteProduct}>
+      <IconButton actionType="delete" />
+    </FormContainer>
+  );
+}
